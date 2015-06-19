ac005d5c269b8853823dfd3a1e8a0bb2a3b9ab8c
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-06-18 23:35:01 -0700

    Made some minor changes, compiles

--- Keepy Uppy/GameViewController.swift
+++ Keepy Uppy/GameViewController.swift
@@ -12,82 +12,53 @@ import SceneKit
 import SpriteKit
 
 class GameViewController: UIViewController {
-
-    override func viewDidLoad()
-    {
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Configure the view.
+        // Configure the view.
         let skView = self.view as! SKView
-        skView.showFPS = true
+        skView.showsFPS = true
         skView.showsNodeCount = true
         
-        //Sprite Kit applies additional optimizations
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
         skView.ignoresSiblingOrder = true
         
-        //Create and configure the scene.
+        // Create and configure the scene.
         let scene = GameScene(size: skView.frame.size)
         skView.presentScene(scene)
         
-        //Pause the view (and thus the game) when the app is interrupted or backgrounded
-        NSNotificationCenter.defautCenter().addObserver(self, selector:)
-    }
-    
-    func handleTap(gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
+        // Pause the view (and thus the game) when the app is interrupted or backgrounded
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleApplicationWillResignActive:", name: UIApplicationWillResignActiveNotification, object: nil)
         
-        // check what nodes are tapped
-        let p = gestureRecognize.locationInView(scnView)
-        if let hitResults = scnView.hitTest(p, options: nil) {
-            // check that we clicked on at least one object
-            if hitResults.count > 0 {
-                // retrieved the first clicked object
-                let result: AnyObject! = hitResults[0]
-                
-                // get its material
-                let material = result.node!.geometry!.firstMaterial!
-                
-                // highlight it
-                SCNTransaction.begin()
-                SCNTransaction.setAnimationDuration(0.5)
-                
-                // on completion - unhighlight
-                SCNTransaction.setCompletionBlock {
-                    SCNTransaction.begin()
-                    SCNTransaction.setAnimationDuration(0.5)
-                    
-                    material.emission.contents = UIColor.blackColor()
-                    
-                    SCNTransaction.commit()
-                }
-                
-                material.emission.contents = UIColor.redColor()
-                
-                SCNTransaction.commit()
-            }
-        }
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleApplicationDidBecomeActive:", name: UIApplicationDidBecomeActiveNotification, object: nil)
     }
     
     override func shouldAutorotate() -> Bool {
         return true
     }
     
-    override func prefersStatusBarHidden() -> Bool {
-        return true
-    }
-    
     override func supportedInterfaceOrientations() -> Int {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
-        } else {
-            return Int(UIInterfaceOrientationMask.All.rawValue)
-        }
+        
+        return Int(UIInterfaceOrientationMask.Portrait.rawValue)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
+    
+    func handleApplicationWillResignActive (note: NSNotification) {
+        
+        let skView = self.view as! SKView
+        skView.paused = true
+    }
+    
+    func handleApplicationDidBecomeActive (note: NSNotification) {
+        
+        let skView = self.view as! SKView
+        skView.paused = false
+    }
+    
 }

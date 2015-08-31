775ecdeb17bff934aea6daa4262026617968ff64
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-08-30 21:54:15 -0700

    Added some Background Images

--- Keepy Uppy/GameScene.swift
+++ Keepy Uppy/GameScene.swift
@@ -11,6 +11,23 @@ import CoreMotion
 
 class GameScene: SKScene
 {
+
+    //Set up the GameScene
+    override init(size: CGSize) {
+        super.init(size: size)
+        
+        anchorPoint = CGPoint(x: 0, y: 1.0)
+        
+        let background = SKSpriteNode(imageNamed: "Bowling Alley.jpg")
+        background.position = CGPoint(x: 0, y: 0)
+        background.anchorPoint = CGPoint(x: 0, y: 1.0)
+        addChild(background)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     var contentCreated = false
     
     override func didMoveToView(view: SKView)
